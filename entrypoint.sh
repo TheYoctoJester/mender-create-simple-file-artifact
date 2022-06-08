a832c1a7cee32d5a2c4b97ae3d51a195744fbe3d
Josef Holzmayr <jester@theyoctojester.info>
2022-06-08 15:02:16 +0000

    rearrange input/output directories in GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,18 @@
 env
 
 cd /home/user/mender-artifact
+
+mkdir -p $GITHUB_WORKSPACE/output
+
 echo "creating the artifact..."
 ./mender-artifact write module-image \
   -T single-file \
   --device-type testdevice1 \
-  -o $GITHUB_WORKSPACE/dist/simpledummy_1.0.mender \
+  -o $GITHUB_WORKSPACE/output/simpledummy_1.0.mender \
   -n simpledummy-1.0 \
   --software-name simpledummy \
   --software-version 1.0 \
-  -f $GITHUB_WORKSPACE/dist/payload
+  -f $GITHUB_WORKSPACE/input/payload
 echo "... done"
 
 ls -alh $GITHUB_WORKSPACE
@@ -21,5 +24,6 @@ echo "uploading the artifact..."
 curl -s -X POST https://hosted.mender.io/api/management/v1/deployments/artifacts \
   -H 'Content-Type: multipart/form-data' \
   -H 'Accept: application/json' \
-  -H "Authorization: Bearer $JWT" $GITHUB_WORKSPACE/dist/payload
+  -H "Authorization: Bearer $JWT" \
+  $GITHUB_WORKSPACE/output/simpledummy_1.0.mender
 echo "... done"
