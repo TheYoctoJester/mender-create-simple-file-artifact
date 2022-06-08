1d1776e61d05e804721fe7c10b4141ceb7fdd20a
Josef Holzmayr <jester@theyoctojester.info>
2022-06-08 15:54:03 +0000

    cleanups!

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,16 @@ cd /home/user/mender-artifact
 sudo mkdir -p $GITHUB_WORKSPACE/output
 sudo chown $(whoami):$(whoami) $GITHUB_WORKSPACE/output
 
+ARTIFACT_NAME=${INPUT_SOFTWARE_NAME}_${INPUT_SOFTWARE_VERSION}.mender
+
 echo "creating the artifact..."
 ./mender-artifact write module-image \
   -T single-file \
-  --device-type testdevice1 \
-  -o $GITHUB_WORKSPACE/output/simpledummy_1.0.mender \
-  -n simpledummy-1.0 \
-  --software-name simpledummy \
-  --software-version 1.0 \
+  --device-type $INPUT_DEVICE_TYPE \
+  -o $GITHUB_WORKSPACE/output/$ARTIFACT_NAME \
+  -n $INPUT_SOFTWARE_NAME-$INPUT_SOFTWARE_VERSION \
+  --software-name $INPUT_SOFTWARE_NAME \
+  --software-version $INPUT_SOFTWARE_VERSION \
   -f $GITHUB_WORKSPACE/input/payload
 echo "... done"
 
@@ -23,5 +25,5 @@ curl -s -X POST https://hosted.mender.io/api/management/v1/deployments/artifacts
   -H 'Content-Type: multipart/form-data' \
   -H 'Accept: application/json' \
   -H "Authorization: Bearer $JWT" \
-  -F artifact=@$GITHUB_WORKSPACE/output/simpledummy_1.0.mender
+  -F artifact=@$GITHUB_WORKSPACE/output/$ARTIFACT_NAME
 echo "... done"
