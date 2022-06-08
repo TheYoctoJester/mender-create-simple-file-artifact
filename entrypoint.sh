d6c6ecc9126891d878737ec9a1782d3a395b0332
Josef Holzmayr <jester@theyoctojester.info>
2022-06-08 13:37:13 +0000

    create artifact in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -l
 
 env
-echo "RUNNER_WORKSPACE in entrypoint.sh: $RUNNER_WORKSPACE"
+
 cd /home/user/mender-artifact
-./mender-artifact -h
-ls -ahl ~
-ls -ahl $RUNNER_WORKSPACE
-tree /github
+./mender-artifact write module-image \
+  -T single-file \
+  --device-type testdevice1 \
+  -o $GITHUB_WORKSPACE/dist/simpledummy_1.0.mender \
+  -n simpledummy-1.0 \
+  --software-name simpledummy \
+  --software-version 1.0 \
+  -f $GITHUB_WORKSPACE/dist/payload
+
+tree $GITHUB_WORKSPACE
